5d378c43724d637c844bad05b0dfa781c37617a5
valentinab25 <valentinab25>
2019-02-20 18:01:46 +0200

    Reformat configuration

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,38 +7,51 @@ if [ "$1" == "/start.sh" ]; then
 
       if [[ -n "$MATRIX_DOMAIN" ]]; then
           echo "Setting matrix domain to $MATRIX_DOMAIN"
-          echo "matrix.domain: $MATRIX_DOMAIN
-dns.overwrite.homeserver.client:
-  - name: '$MATRIX_DOMAIN'
+          echo "matrix:
+  domain: $MATRIX_DOMAIN
+dns:
+  overwrite:
+    homeserver:
+      client:
+        - name: '$MATRIX_DOMAIN'
     value: 'http://matrix:8008' " >> "$CONF_FILE_PATH"
       fi
 
       if [[ -n "$SIGN_KEY_PATH" ]]; then
           echo "Setting signing key path to $SIGN_KEY_PATH"
-          echo "key.path: $SIGN_KEY_PATH" >> "$CONF_FILE_PATH"
+          echo "key:
+  path: $SIGN_KEY_PATH" >> "$CONF_FILE_PATH"
       fi
       touch $SIGN_KEY_PATH
       if [[ -n "$SQLITE_DATABASE_PATH" ]]; then
           echo "Setting SQLite DB path to $SQLITE_DATABASE_PATH"
-          echo "storage.provider.sqlite.database: $SQLITE_DATABASE_PATH" >> "$CONF_FILE_PATH"
+          echo "storage:
+  provider:
+    sqlite:
+      database: $SQLITE_DATABASE_PATH" >> "$CONF_FILE_PATH"
       fi
 
 
       if [[ -n "${LDAP_HOST}" ]]; then
           echo "Setting LDAP"
-          echo "ldap.enabled: true
-ldap.connection.host: '${LDAP_HOST}'
-ldap.connection.bindDn: '${LDAP_BINDDN}'
-ldap.connection.bindPassword: '${LDAP_BINDDN_PASS}'
-ldap.connection.baseDn: '${LDAP_BASEDN}'
-ldap.connection.tls: '${LDAP_TLS}'
-ldap.connection.port: '${LDAP_PORT}'
-ldap.attribute.uid.type: 'uid'
-ldap.attribute.uid.value: 'uid'
-ldap.attribute.name: 'cn' "  >> "$CONF_FILE_PATH"
+          echo "ldap:
+  enabled: true
+  connection:
+    host: '${LDAP_HOST}'
+    bindDn: '${LDAP_BINDDN}'
+    bindPassword: '${LDAP_BINDDN_PASS}'
+    baseDn: '${LDAP_BASEDN}'
+    tls: '${LDAP_TLS}'
+    port: '${LDAP_PORT}'
+  attribute:
+    uid:
+      type: 'uid'
+      value: 'uid'
+    name: 'cn' "  >> "$CONF_FILE_PATH"
 
            if [[ -n "$LDAP_FILTER" ]]; then
-               echo "ldap.filter: '($LDAP_FILTER)'" >> "$CONF_FILE_PATH"
+               echo "ldap:
+  filter: '($LDAP_FILTER)'" >> "$CONF_FILE_PATH"
            fi
       fi
 
